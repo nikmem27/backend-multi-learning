251afd3c8bf79bed56c7c0e405b595515a5cfcb8
Nikos Memos <memnik@hotmail.com>
2020-09-26 00:45:30 +0300

    Final Commit

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var testsRouter = require('./routes/tests');
 
 var app = express();
 
@@ -19,16 +21,19 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+app.use(cors()); // Use this after the variable declaration
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('./tests', testsRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,33 @@
 var express = require('express');
 var router = express.Router();
+const { Pool, Client } = require("pg");
+
+const pool = new Pool({
+  user: "postgres",
+  host: "localhost",
+  database: "postgres",
+  password: "memos",
+  port: "5432"
+});
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
 
-router.post('/', function(req, res, next) {
-  res.send(req.body);
+router.post('/', function (req, res, next) {
+  //res.send(req.body);
+  pool.query(`insert into users(username, password) values ('${req.body.username}', '${req.body.password}')`, (err, resp) => {
+    // check if the response is not 'undefined'
+    if (err) {
+      res.statusCode = 500;
+      res.setHeader('Content-Type', 'application/json');
+      res.json({ err: err });
+    }
+    res.statusCode = 200;
+    res.send({status:'Success'});
+
+  });
 });
 
 module.exports = router;
